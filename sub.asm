5d810e62fe72c0a7f7cc2b4f94c296dcbcd777c0
ankitkumarbrur <38562921+ankitkumarbrur@users.noreply.github.com>
2019-10-28 18:23:36 +0530

    Update sub.asm

--- sub.asm
+++ sub.asm
@@ -1,23 +1,59 @@
 .model small
 .stack 100h
 .data
-        a dw 07867h,03776h
-	b dw 05778h,06656h
+        a dw 0000h,4678h
+	b dw 0000h,5321h
 	temp1 dw ?
 	temp2 dw ?
+	flag  dw 0h
 .code
 .startup
-	mov ax,a[2]
+
+	mov ax,a[0]
+	mov bx,b[0]
+	cmp ax,bx
+	jae skip1
+	mov flag,1h
+
+  skip1:cmp ax,0h
+	ja skip2	
+	cmp bx,0h
+	ja skip2
+	mov flag,2h
+
+  skip2:mov ax,a[2]
 	mov bx,b[2]
-	sub ax,bx
+
+	cmp flag,2h
+	jb skip3
+	cmp ax,bx
+	jae skip3
+	mov flag,1h
+	
+  skip3:sub ax,bx
 	mov temp1,ax
 
 	mov ax,a[0]
 	mov bx,b[0]
-	sbb ax,bx
+
+ 	sbb ax,bx
 	mov temp2,ax
 
-	mov cx,4h
+	cmp flag,1h
+	ja skip4
+	mov ax,temp1
+	neg ax
+	mov temp1,ax
+	mov ax,temp2
+	neg ax
+	sub ax,1
+	mov temp2,ax
+	mov dl,45
+	mov ah,02h
+	int 21h
+	
+
+  skip4:mov cx,4h
         mov bx,temp2
      t2:rol bx,4
 	mov dx,bx
@@ -50,12 +86,3 @@
 
 .exit
 end
-
-
-
-
-
-
-
-	
-		
